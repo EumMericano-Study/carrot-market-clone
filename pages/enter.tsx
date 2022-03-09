1b1c8c7258f3bdf4a300c0c1b744f1a74e03e4df
Eumericano <opwer032@naver.com>
2022-03-09 10:32:04 +0900

    feat: Init enter page

---
+++ pages/enter.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import GithubIcon from "../components/icons/github";
+
+export default function Enter() {
+  const [method, setMethod] = useState<"email" | "phone">("email");
+  const setToEmail = () => setMethod("email");
+  const setToPhone = () => setMethod("phone");
+  return (
+    <div>
+      <h3>Enter to Carrot</h3>
+      <div>
+        <h5>Enter using</h5>
+        <div>
+          <button onClick={setToEmail}>Email</button>
+          <button onClick={setToPhone}>Phone</button>
+        </div>
+      </div>
+      <form>
+        <label>
+          {method === "email" && "Email address"}
+          {method === "phone" && "Phone number"}
+        </label>
+        <div>
+          {method === "email" && <input type="email" required />}
+          {method === "phone" && (
+            <div>
+              <span>+82</span>
+              <input type="number" required />
+            </div>
+          )}
+        </div>
+        <button>
+          {method === "email" && "Get login link"}
+          {method === "phone" && "Get one-time password"}
+        </button>
+      </form>
+      <div>
+        <div>
+          <div />
+          <div>
+            <span>Or Enter With</span>
+          </div>
+        </div>
+        <div>
+          <button></button>
+          <button>
+            <GithubIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-blue-500">It works</h1>
+    <div className="bg-slate-400 py-10 px-5">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
