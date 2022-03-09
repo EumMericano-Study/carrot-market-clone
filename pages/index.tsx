983a61a581a327a736358fd29817335a3a5d0f61
Eumericano <opwer032@naver.com>
2022-03-09 16:59:26 +0900

    style: Set home style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,41 @@
 import type { NextPage } from "next";
+import { HeartIcon, ChatBubbleIcon, Plus } from "../components/icons";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-10 px-5">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className="flex flex-col space-y-5 py-10">
+      {Array(10)
+        .fill("")
+        .map((_, index) => (
+          <div
+            key={index}
+            className="flex justify-between cursor-pointer px-4 pb-4 border-b"
+          >
+            <div className="flex space-x-4">
+              <div className="w-20 h-20 bg-gray-400 rounded-md" />
+              <div className="flex flex-col pt-2">
+                <h3 className="text-sm font-medium text-gray-900">
+                  New Iphone 14
+                </h3>
+                <span className="text-xs text-gray-500">Black</span>
+                <span className="font-medium text-gray-900 mt-2">$95</span>
+              </div>
+            </div>
+            <div className="flex justify-end items-end space-x-2">
+              <div className="flex items-center text-sm text-gray-500 space-x-0.5">
+                <HeartIcon />
+                <span>1</span>
+              </div>
+              <div className="flex items-center text-sm text-gray-500 space-x-0.5">
+                <ChatBubbleIcon />
+                <span>1</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      <button className="fixed bottom-24 right-10 bg-orange-500 text-white transition-colors cursor-pointer shadow-xl p-4 rounded-full hover:bg-orange-600">
+        <Plus className="w-7 h-7" />
+      </button>
     </div>
   );
 };
