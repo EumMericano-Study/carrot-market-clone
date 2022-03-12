4523bc99fbb38945251f89253fae0a702fb1ce3c
Eumericano <opwer032@naver.com>
2022-03-12 10:47:40 +0900

    style: Set chat detail style

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -1,23 +1,34 @@
 export default function ChatDetail() {
   return (
-    <div>
-      <div>
-        <div />
-        <div>Hi, How much are you selling them for?</div>
+    <div className="px-4 py-10 space-y-4 ">
+      <div className="flex items-start space-x-2">
+        <div className="w-8 h-8 bg-slate-400 rounded-full" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+          <p>Hi, How much are you selling them for?</p>
+        </div>
       </div>
-      <div>
-        <div />
-        <div>I want ￦20,000</div>
+      <div className="flex items-start flex-row-reverse space-x-2 space-x-reverse">
+        <div className="w-8 h-8 bg-slate-400 rounded-full" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+          <p>I want ￦20,000</p>
+        </div>
       </div>
-      <div>
-        <div />
-        <div>미쳤어</div>
+      <div className="flex items-start space-x-2">
+        <div className="w-8 h-8 bg-slate-400 rounded-full" />
+        <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+          <p>미쳤어</p>
+        </div>
       </div>
-      <div>
-        <div>
-          <input type="text" />
-          <div>
-            <span>&rarr;</span>
+      <div className="w-full max-w-md fixed bottom-2 inset-x-0 mx-auto">
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            className="w-full shadow-sm pr-12 rounded-full border-gray-300 focus:outline-none focus:border-orange-500 focus:ring-orange-500"
+          />
+          <div className="absolute flex inset-y-0 right-0 py-1.5 pr-1.5">
+            <button className="flex items-center bg-orange-500 text-sm text-white cursor-pointer px-3 rounded-full hover:bg-orange-600 focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+              &rarr;
+            </button>
           </div>
         </div>
       </div>
