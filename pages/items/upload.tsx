c550fe3a70831ef85d71242ee426ec9d536a016e
Eumericano <opwer032@naver.com>
2022-03-14 12:43:03 +0900

    feat: Add name and change style

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -2,7 +2,7 @@ import { AddPhotoIcon } from "../../components/icons";
 
 export default function Upload() {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 py-16 space-y-5">
       <div>
         <label
           htmlFor="photo"
@@ -12,7 +12,22 @@ export default function Upload() {
           <input id="photo" type="file" accept="image/*" className="hidden" />
         </label>
       </div>
-      <div className="py-5">
+      <div>
+        <label
+          htmlFor="name"
+          className="block text-sm text-gray-700 font-medium mb-1"
+        >
+          Name
+        </label>
+        <div className="relative flex items-center shadow-sm rounded-md">
+          <input
+            id="name"
+            type="text"
+            className="appearance-none w-full shadow-sm placeholder-gray-400 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+          />
+        </div>
+      </div>
+      <div>
         <label
           htmlFor="price"
           className="block text-sm text-gray-700 font-medium mb-1"
@@ -47,7 +62,7 @@ export default function Upload() {
           className="w-full shadow-sm mt-1 border-gray-300 rounded-md focus:ring-orange-500 focus:border-orange-500"
         />
       </div>
-      <button className="w-full bg-orange-500 text-white text-sm font-medium shadow-sm transition px-4 py-2 mt-5 border border-transparent rounded-md hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+      <button className="w-full bg-orange-500 text-white text-sm font-medium shadow-sm transition px-4 py-2 border border-transparent rounded-md hover:bg-orange-600 focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
         Upload Product
       </button>
     </div>
