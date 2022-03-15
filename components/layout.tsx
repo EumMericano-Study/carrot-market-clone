8a3f23a48c8572ae38a4021a7dc9565fa7039084
Eumericano <opwer032@naver.com>
2022-03-15 19:07:55 +0900

    feat: Add common layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,12 @@
+import { cls } from "../libs/utils";
+import {
+  ChatBubbleIcon,
+  HomeIcon,
+  NewsIcon,
+  UserIcon,
+  VideoIcon,
+} from "./icons";
+
 interface LayoutProps {
   title?: string;
   canGoBack?: boolean;
@@ -13,11 +22,36 @@ export default function Layout({
 }: LayoutProps) {
   return (
     <div>
-      <div className="w-full fixed top-0 flex items-center bg-white text-lg font-medium text-gray-700 py-4 border-b">
+      <div className="w-full max-w-lg fixed top-0 flex justify-center items-center bg-white text-lg font-medium text-gray-700 py-3 border-b">
         {title && <span>{title}</span>}
       </div>
-      {children}
-      {hasTabBar && <nav></nav>}
+      <div className={cls("pt-[4.5rem]", hasTabBar ? "pb-24" : "")}>
+        {children}
+      </div>
+      {hasTabBar && (
+        <nav className="w-full max-w-lg fixed bottom-0 flex justify-between items-center bg-white text-gray-800 border-t px-10 pb-5 pt-3">
+          <div className="flex flex-col items-center space-y-2">
+            <HomeIcon />
+            <span>Home</span>
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <NewsIcon />
+            <span>Town</span>
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <ChatBubbleIcon className="w-6 h-6" />
+            <span>Chat</span>
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <VideoIcon />
+            <span>Live</span>
+          </div>
+          <div className="flex flex-col items-center space-y-2">
+            <UserIcon />
+            <span>Profile</span>
+          </div>
+        </nav>
+      )}
     </div>
   );
 }
