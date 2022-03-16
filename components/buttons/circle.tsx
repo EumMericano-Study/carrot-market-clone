679cf92932aeee980ffc92601b2fcbb9c3561709
Eumericano <opwer032@naver.com>
2022-03-16 09:35:51 +0900

    feat: Add circle button focus

--- components/buttons/circle.tsx
+++ components/buttons/circle.tsx
@@ -9,7 +9,7 @@ export default function CircleButton({ className = "", children }: Props) {
   return (
     <button
       className={cls(
-        "w-14 h-14 flex justify-center items-center bg-orange-500 text-white border-none rounded-full hover:bg-orange-600",
+        "w-14 h-14 flex justify-center items-center bg-orange-500 text-white border-none rounded-full hover:bg-orange-600 focus:outline-none focus:bg-orange-600",
         className
       )}
     >
