a22994b08c80dd3f3fbd1ae4c129559df4a9967e
Eumericano <opwer032@naver.com>
2022-06-18 18:31:04 +0900

    feat: Check valid and value

--- pages/[etc]react_hook_form.tsx
+++ pages/[etc]react_hook_form.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 
 // react hook form
 // Better Validation
@@ -7,31 +7,62 @@ import { useForm } from "react-hook-form";
 // Better input controll
 // Never deal events
 
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
+
 const UsingUseFormHook = () => {
-  const { register, watch } = useForm();
+  const { register, watch, handleSubmit } = useForm<LoginForm>();
   /**
    * resgiter('name') 을 출력해보면
    * name, onBlur, onChange, ref 항목을 담고 있다.
+   * name을 통해 변수를 지정하고 onChange뿐 아니라 ref, onblur 처리들이
+   * 기본적으로 설정되어 있기 때문에 간편하게 사용가능하다.
    */
   /**
    * watch()를 출력해보면
    * useForm을 통해 명시된 value들을 객체형태로 모아 볼 수 있다.
    * {usename: 'bar', email:"bar@foo.com", password: "foo"}
    */
+  /**
+   * handleSubmit은 두가지 인자를 받는다
+   * 첫 번째 인자는 submit이 validate할때 실행되므로 필수 입력 항목이며
+   * 두 번째 인자는 submit이 invalidate 되었을 떄 실행되는 항목이다.
+   */
+
+  const onValid = (data: LoginForm) => {
+    alert(confirm("onValid"));
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: "username은 필수정보 입니다.",
+          // reqired: true로 어떤 오류가 있는지 체크한 후 출력도 가능하지만
+          // string 형태로 에러가 true일때 보여줄 message를 함께 출력 할 수 있다.
+          minLength: {
+            message: "username은 5글자 보다 길어야 합니다.",
+            value: 5,
+          },
+          // 숫자 값만 가능한 length valid값을 객체를 통해 메시지까지 전송할 수 있다.
+        })}
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", { required: "이메일은 필수정보 입니다." })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: "비밀번호는 필수정보 입니다." })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value="제출 하기" />
     </form>
